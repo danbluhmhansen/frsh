83fe7497cc59b7ff8811830df38041f1dd2d25e0
Dan Bluhm Hansen <16237377+danbluhmhansen@users.noreply.github.com>
2023-08-20 08:32:33 +0200

    misc

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -8,7 +8,7 @@ interface CounterProps {
 export default function Counter(props: CounterProps) {
   return (
     // @ts-ignore: attributify
-    <div flex gap="8" p="y-6">
+    <div flex="~ row" gap="8" p="y-6">
       <Button onClick={() => props.count.value -= 1}>-1</Button>
       {/* @ts-ignore: attributify */}
       <p text="3xl">{props.count}</p>
--- routes/games/[gameSlug]/actors/[actorKindSlug].tsx
+++ routes/games/[gameSlug]/actors/[actorKindSlug].tsx
@@ -2,22 +2,15 @@ import { defineRoute } from "$fresh/server.ts";
 import postgres from "postgresjs";
 import Actor from "~models/actor.ts";
 import ActorKind from "~models/actorKind.ts";
-import { Breadcrumbs } from "~components/Breadcrumbs.tsx";
-import Game from "~models/game.ts";
 
-export default defineRoute(async (_, { params: { gameSlug, actorKindSlug }, url }) => {
+export default defineRoute(async (_, { params: { gameSlug, actorKindSlug } }) => {
   const sql = postgres();
-  const [game] = await sql<Game[]>`SELECT name FROM game WHERE slug = ${gameSlug}`;
   const [actorKind] = await sql<ActorKind[]>`SELECT id, name FROM actor_kind WHERE slug = ${actorKindSlug}`;
   const actors = await sql<Actor[]>`SELECT name, slug FROM actor WHERE kind_id = ${actorKind.id}`;
   return (
     <>
-      <Breadcrumbs url={url}>
-        <span>{game.name}</span>
-        <span>Actors</span>
-      </Breadcrumbs>
       {/* @ts-ignore: attributify */}
-      <h2 text="xl">Actors</h2>
+      <a href={`/games/${gameSlug}/actors/${actorKindSlug}`} text="3xl" font="bold">{actorKind.name}</a>
       <ul>
         {actors.map((actor) => (
           <li>
